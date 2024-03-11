126fe6b8976b6fd9d428871f1765dd98fa51ed37
greg <go2432@wayne.edu>
2024-03-10 23:02:14 -0500

    created directories for wolf

---
+++ SPCE/WAIBEL_2019_DSP_RC_14_AL_12/templates/grid.sh
@@ -0,0 +1,34 @@
+{% extends "slurm.sh" %}
+
+{% block header %}
+{% set gpus = operations|map(attribute='directives.ngpu')|sum %}
+    {{- super () -}}
+
+{% if gpus %}
+#SBATCH -q gpu
+#SBATCH --gres gpu:{{ gpus }}
+{%- else %}
+#SBATCH -q primary
+{%- endif %}
+
+#SBATCH -N 1
+
+
+echo  "Running on host" hostname
+echo  "Time is" date
+conda activate mosdef_gomc 
+
+module load python/3.8
+
+{% if gpus %}
+module load cuda/10.0
+nvidia-smi
+{%- endif %}
+
+{% endblock header %}
+
+{% block body %}
+    {{- super () -}}
+
+
+{% endblock body %}
